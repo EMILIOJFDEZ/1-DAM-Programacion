9c8958c0081adfab06e525de5d1c6588238a324c
EmilioJFdez <99140001+EMILIOJFDEZ@users.noreply.github.com>
2022-11-07 20:01:03 +0100

    Add files via upload

---
+++ 01_ExamenTema1/Examen2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Examen2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("****************************************");
+    System.out.println("*    Emilio Jose Fernandez Fernandez   *");
+    System.out.println("****************************************");
+    }
+    
+}
