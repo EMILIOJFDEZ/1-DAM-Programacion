74000ccdb7bdd1d7cf4c89769957dcee1f6bcdba
EmilioJFdez <99140001+EMILIOJFDEZ@users.noreply.github.com>
2022-11-21 20:40:51 +0100

    Create Prueba01.java

---
+++ 03_EjerciciosT3/Prueba/Prueba01.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_3_prueba_01;
+
+/**
+ *
+ * @author Emilio<emiliojfdez2004@gmail.com>
+ */
+public class Ejercicio_3_Prueba_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        int n1 =Integer.parseInt(args[0]);
+        int n2 =Integer.parseInt(args[1]);
+        int n3=n1+n2;
+        System.out.println(n3);
+    }
+}
