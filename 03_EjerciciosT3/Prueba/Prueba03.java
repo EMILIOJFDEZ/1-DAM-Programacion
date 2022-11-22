cd1af557415a4066e5b0a77e8fe4bd7943724a03
EmilioJFdez <99140001+EMILIOJFDEZ@users.noreply.github.com>
2022-11-22 18:08:14 +0100

    Add files via upload

--- 03_EjerciciosT3/Prueba/Prueba03.java
+++ 03_EjerciciosT3/Prueba/Prueba03.java
@@ -15,11 +15,36 @@ public class Ejercicio_3_Prueba_03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String nombreSO="os.name";
-        String versionSO="os.version";
-        String arquitecturaSO="os.arch";
-        System.out.println("\nNombre del SO: " +System.getProperty(nombreSO));
-        System.out.println("\nVersion del SO: " +System.getProperty(versionSO));
-        System.out.println("\nArquitectura del SO: " +System.getProperty(arquitecturaSO));
+        String fileseparator="file.separator";
+        String classpath="java.class.path";
+        String javahome="java.home";
+        String javavendor="java.vendor";
+        String javavendorurl="java.vendor.url";
+        String javaversion="java.version";
+        String lineseparator="line.separator";
+        String osarch="os.arch";
+        String osname="os.name";
+        String osversion="os.version";
+        String pathseparator="path.separator";
+        String userdir="user.dir";
+        String userhome="user.home";
+        String username="user.name";
+        
+        System.out.println("Separador de lineas: "+System.getProperty(fileseparator));
+        System.out.println("Ubicacion del proyecto: "+System.getProperty(classpath));
+        System.out.println("Ubicacion home de java: "+System.getProperty(javahome));
+        System.out.println("Distribuidor del programa: "+System.getProperty(javavendor));
+        System.out.println("Enlace del distribuidor: "+System.getProperty(javavendorurl));
+        System.out.println("Version java: "+System.getProperty(javaversion));
+        System.out.println("Separador: "+System.getProperty(lineseparator));
+        System.out.println("Arquitectura del SO: "+System.getProperty(osarch));
+        System.out.println("Nombre del SO: "+System.getProperty(osname));
+        System.out.println("Version del SO: "+System.getProperty(osversion));
+        System.out.println("Separador de comando: "+System.getProperty(pathseparator));
+        System.out.println("Ubicacion del directorio: "+System.getProperty(userdir));
+        System.out.println("Ubicacion home: "+System.getProperty(userhome));
+        System.out.println("Ubicacion nombre de usuario: "+System.getProperty(username));
+        
     }
+    
 }
